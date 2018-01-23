4bc8685f5548143cb0bd1ffd37295dd24a907a4d
Heiko Mathes <heiko.mathes@gmail.com>
2018-01-23 18:04:10 +0100

    :sparkles: add method to compare error types

--- src/is_error.ts
+++ src/is_error.ts
@@ -1,5 +1,13 @@
 import {BaseError} from './base_error';
 
-export function isError(error: any): error is BaseError {
+export function isEssentialProjectsError(error: any): error is BaseError {
   return (error instanceof Error) && (error as BaseError).isEssentialProjectsError === true;
+}
+
+export function isError(error: any, errorClass: BaseError): boolean {
+  if (!isEssentialProjectsError(error)) {
+    return false;
+  }
+
+  return error.constructor.name === errorClass.name;
 }
