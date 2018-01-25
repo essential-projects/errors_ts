facfbd0157e792e8f432970aa2f5ec20a858d58b
Heiko Mathes <heiko.mathes@gmail.com>
2018-01-25 09:19:33 +0100

    :bug: handle derived error classes correctly

--- src/base_error.ts
+++ src/base_error.ts
@@ -1,4 +1,7 @@
+import {Logger} from 'loggerhythm';
 import * as errorClasses from './index';
+
+const logger: Logger = Logger.createLogger('essential-projects-errors');
 
 export class BaseError extends Error {
   protected _code: number;
@@ -12,7 +15,24 @@ export class BaseError extends Error {
   }
 
   public static deserialize(serializedError: string): BaseError {
-    const errorInfo: any = JSON.parse(serializedError);
+    let errorInfo: any;
+
+    try {
+      errorInfo = JSON.parse(serializedError);
+    } catch (error) {
+      throw new Error(`Error while deserializing an essential-projects-error: Couldn't parse string: ${error.message}`);
+    }
+
+    if (errorInfo.errorClassName === undefined
+     || errorInfo.code === undefined
+     || errorInfo.callStack === undefined) {
+      throw new Error('Error while deserializing an essential-projects-error: Serialized object has the wrong structure.');
+    }
+
+    if (errorClasses[errorInfo.errorClassName] === undefined) {
+      throw new Error(`Error while deserializing an essential-projects-error: ${errorInfo.errorClassName} is not a known essential-projects-error`);
+    }
+
     const errorClass: BaseError = new errorClasses[errorInfo.errorClassName](errorInfo.code, errorInfo.message);
     errorClass.stack = errorInfo.callStack;
     errorClass.additionalInformation = errorInfo.additionalInformation;
@@ -37,8 +57,38 @@ export class BaseError extends Error {
   }
 
   public serialize(): string {
+
+    let essentialProjectsErrorName: string = this.constructor.name;
+    if (errorClasses[this.constructor.name] === undefined) {
+      // the error we want to serailize is not a known essential-projects-error.
+      // It might extend one, and if so, we need to find the essential-projects-
+      // error this one is based on.
+
+      // tslint:disable-next-line:no-this-assignment
+      let classToCheck: any = this;
+
+      while (classToCheck !== null) {
+        const currentClassInPrototypeChainIsSearchedFor: Boolean = classToCheck.constructor !== undefined
+                                                                && errorClasses[classToCheck.constructor.name] !== undefined;
+        if (currentClassInPrototypeChainIsSearchedFor) {
+          // We found the essential-projects-error this one is based on.
+          essentialProjectsErrorName = classToCheck.constructor.name;
+          break;
+        }
+
+        classToCheck = Object.getPrototypeOf(classToCheck);
+      }
+
+      if (classToCheck === null) {
+        // tslint:disable-next-line:max-line-length
+        throw new Error(`'serialize' was called on an error that extends an essential-projects BaseError, but no known essential-projects error was found in its prototype-chain. THIS SHOULD NEVER HAPPEN! Are you using mutliple versions of @essential-projects/errors_ts in your project?`);
+      }
+
+      logger.info(`derived error '${this.constructor.name}' will be serialized as '${essentialProjectsErrorName}'`);
+    }
+
     return JSON.stringify({
-      errorClassName: this.constructor.name,
+      errorClassName: essentialProjectsErrorName,
       code: this.code,
       message: this.message,
       callStack: this.stack,
