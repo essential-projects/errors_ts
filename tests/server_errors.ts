78c5b263c073a38b9ea5f924fb752e11b739763d
Miguel Carballo Garcia <miguel.garcia@5minds.de>
2018-02-02 10:35:57 +0100

    :lipstick: Add shouldjs to server_errors

--- tests/server_errors.ts
+++ tests/server_errors.ts
@@ -1,9 +1,8 @@
-import 'mocha';
-
 import {
   BadGatewayError,
   BandwithLimitExceededError,
   BaseError,
+  ErrorCodes,
   GatewayTimeoutError,
   InsufficientStorageError,
   InternalServerError,
@@ -14,7 +13,8 @@ import {
   VersionNotSupportedError,
 } from '../src/index';
 
-import {ErrorCodes} from '../src/error_codes';
+import 'mocha';
+import * as should from 'should';
 
 let error: BaseError;
 
@@ -23,9 +23,7 @@ describe('server_errors', () => {
     it(`should return ${ErrorCodes.BadGatewayError}`, () => {
       error = new BadGatewayError('BadGatewayError');
       const result: number = error.code;
-      if (result !== ErrorCodes.BadGatewayError) {
-        throw new Error(`Expected error Code ${ErrorCodes.BadGatewayError} but was ${result}.`);
-      }
+      should(result).be.equal(ErrorCodes.BadGatewayError);
     });
   });
 
@@ -33,9 +31,7 @@ describe('server_errors', () => {
     it(`should return ${ErrorCodes.BandwithLimitExceededError}`, () => {
       error = new BandwithLimitExceededError('BandwithLimitExceededError');
       const result: number = error.code;
-      if (result !== ErrorCodes.BandwithLimitExceededError) {
-        throw new Error(`Expected error Code ${ErrorCodes.BandwithLimitExceededError} but was ${result}.`);
-      }
+      should(result).be.equal(ErrorCodes.BandwithLimitExceededError);
     });
   });
 
@@ -43,9 +39,7 @@ describe('server_errors', () => {
     it(`should return ${ErrorCodes.GatewayTimeoutError}`, () => {
       error = new GatewayTimeoutError('GatewayTimeoutError');
       const result: number = error.code;
-      if (result !== ErrorCodes.GatewayTimeoutError) {
-        throw new Error(`Expected error Code ${ErrorCodes.GatewayTimeoutError} but was ${result}.`);
-      }
+      should(result).be.equal(ErrorCodes.GatewayTimeoutError);
     });
   });
 
@@ -53,9 +47,7 @@ describe('server_errors', () => {
     it(`should return ${ErrorCodes.InsufficientStorageError}`, () => {
       error = new InsufficientStorageError('InsufficientStorageError');
       const result: number = error.code;
-      if (result !== ErrorCodes.InsufficientStorageError) {
-        throw new Error(`Expected error Code ${ErrorCodes.InsufficientStorageError} but was ${result}.`);
-      }
+      should(result).be.equal(ErrorCodes.InsufficientStorageError);
     });
   });
 
@@ -63,9 +55,7 @@ describe('server_errors', () => {
     it(`should return ${ErrorCodes.InternalServerError}`, () => {
         error = new InternalServerError('InternalServerError');
         const result: number = error.code;
-        if (result !== ErrorCodes.InternalServerError) {
-          throw new Error(`Expected error Code ${ErrorCodes.InternalServerError} but was ${result}.`);
-        }
+        should(result).be.equal(ErrorCodes.InternalServerError);
     });
   });
 
@@ -73,9 +63,7 @@ describe('server_errors', () => {
     it(`should return ${ErrorCodes.LoopDetectedError}`, () => {
       error = new LoopDetectedError('LoopDetectedError');
       const result: number = error.code;
-      if (result !== ErrorCodes.LoopDetectedError) {
-        throw new Error(`Expected error Code ${ErrorCodes.LoopDetectedError} but was ${result}.`);
-      }
+      should(result).be.equal(ErrorCodes.LoopDetectedError);
     });
   });
 
@@ -83,9 +71,7 @@ describe('server_errors', () => {
     it(`should return ${ErrorCodes.NetworkAuthenticationRequiredError}`, () => {
       error = new NetworkAuthenticationRequiredError('NetworkAuthenticationRequiredError');
       const result: number = error.code;
-      if (result !== ErrorCodes.NetworkAuthenticationRequiredError) {
-        throw new Error(`Expected error Code ${ErrorCodes.NetworkAuthenticationRequiredError} but was ${result}.`);
-      }
+      should(result).be.equal(ErrorCodes.NetworkAuthenticationRequiredError);
     });
   });
 
@@ -93,9 +79,7 @@ describe('server_errors', () => {
     it(`should return ${ErrorCodes.NotImplementedError}`, () => {
       error = new NotImplementedError('NotImplementedError');
       const result: number = error.code;
-      if (result !== ErrorCodes.NotImplementedError) {
-        throw new Error(`Expected error Code ${ErrorCodes.NotImplementedError} but was ${result}.`);
-      }
+      should(result).be.equal(ErrorCodes.NotImplementedError);
     });
   });
 
@@ -103,9 +87,7 @@ describe('server_errors', () => {
     it(`should return ${ErrorCodes.ServiceUnavaliableError}`, () => {
       error = new ServiceUnavaliableError('ServiceUnavaliableError');
       const result: number = error.code;
-      if (result !== ErrorCodes.ServiceUnavaliableError) {
-        throw new Error(`Expected error Code ${ErrorCodes.ServiceUnavaliableError} but was ${result}.`);
-      }
+      should(result).be.equal(ErrorCodes.ServiceUnavaliableError);
     });
   });
 
@@ -113,9 +95,7 @@ describe('server_errors', () => {
     it(`should return ${ErrorCodes.VersionNotSupportedError}`, () => {
       error = new VersionNotSupportedError('VersionNotSupportedError');
       const result: number = error.code;
-      if (result !== ErrorCodes.VersionNotSupportedError) {
-        throw new Error(`Expected error Code ${ErrorCodes.VersionNotSupportedError} but was ${result}.`);
-      }
+      should(result).be.equal(ErrorCodes.VersionNotSupportedError);
     });
   });
 });
